8a33ae643362436ea664ef65dd385dbfa02be176
J-DK <dileepkumarjami@gmail.com>
2021-03-11 16:43:43 +0530

    feat: added provider dependency to provide the changes to the wrapper widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/models/user.dart';
 import 'package:flutter_firebase/screens/wrapper.dart';
+import 'package:flutter_firebase/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +15,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<BrewUser>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/models/user.dart';
 import 'package:flutter_firebase/screens/authenticate/authenticate.dart';
+import 'package:flutter_firebase/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final user = Provider.of<BrewUser>(context);
+    print(user);
     // return either home or authenticate widget
-    return Authenticate();
+    return user == null ? Authenticate() : Home();
   }
 }
