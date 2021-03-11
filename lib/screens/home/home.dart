a8d73219a3cbccb50610b999077a95b5bd18e691
J-DK <dileepkumarjami@gmail.com>
2021-03-11 21:11:10 +0530

    feat: added brew list widget to show the brew items

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase/models/brew.dart';
+import 'package:flutter_firebase/screens/home/brew_list.dart';
 import 'package:flutter_firebase/services/auth.dart';
+import 'package:flutter_firebase/services/database.dart';
+import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
 
@@ -7,21 +11,25 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.brown[50],
-      appBar: AppBar(
-        title: Text('Brew Crew'),
-        backgroundColor: Colors.brown[400],
-        elevation: 0,
-        actions: <Widget>[
-          FlatButton.icon(
-              onPressed: () async {
-                await _auth.signOut();
-              },
-              icon: Icon(Icons.person),
-              label: Text('Logout')
-          )
-        ],
+    return StreamProvider<List<Brew>>.value(
+      value: DatabaseService().brews,
+      child: Scaffold(
+        backgroundColor: Colors.brown[50],
+        appBar: AppBar(
+          title: Text('Brew Crew'),
+          backgroundColor: Colors.brown[400],
+          elevation: 0,
+          actions: <Widget>[
+            FlatButton.icon(
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+                icon: Icon(Icons.person),
+                label: Text('Logout')
+            )
+          ],
+        ),
+        body: BrewList(),
       ),
     );
   }
