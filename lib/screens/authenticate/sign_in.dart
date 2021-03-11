57b608fb5836c97260a78be9a54932dc33f7729b
J-DK <dileepkumarjami@gmail.com>
2021-03-11 17:05:08 +0530

    feat: added text form fields for email & password

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -8,6 +8,9 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
 
+  String email = '';
+  String password = '';
+
   final AuthService _auth = AuthService();
 
   @override
@@ -21,18 +24,39 @@ class _SignInState extends State<SignIn> {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20, horizontal: 50),
-        child: RaisedButton(
-          child: Text('Sign in anon'),
-          onPressed: () async {
-            dynamic result = await _auth.signInAnonymous();
-            if (result == null) {
-              print('error signing in');
-            } else {
-              print('signed in');
-              print(result);
-            }
-          },
-        ),
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              TextFormField(
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                color: Colors.brown[900],
+                child: Text(
+                    'Sign In',
+                    style: TextStyle(
+                      color: Colors.white
+                    )
+                ),
+                onPressed: () {
+                  print(email);
+                  print(password);
+                }
+              )
+            ],
+          ),
+        )
       ),
     );
   }
